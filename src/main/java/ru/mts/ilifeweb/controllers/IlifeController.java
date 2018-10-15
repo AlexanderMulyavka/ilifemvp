8bf1f92b36b3d3cebedc8eb7ab5636b405d7701d
AlexanderMulyavka <alexander.mulyavka@gmail.com>
2018-10-15 17:03:32 +0300

    Update IlifeController.java

--- src/main/java/ru/mts/ilifeweb/controllers/IlifeController.java
+++ src/main/java/ru/mts/ilifeweb/controllers/IlifeController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.mts.ilifeweb.service.PushNotificationsService;
 
 import java.util.concurrent.CompletableFuture;
